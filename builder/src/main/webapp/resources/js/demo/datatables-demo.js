2c3ce9042d2a32667cd0b7ba78a056e1b8eba585
cha2hyun <cha2hyun@172.16.40.107>
2020-05-28 17:03:00 +0900

    Soohyun First Commit Test

---
+++ builder/src/main/webapp/resources/js/demo/datatables-demo.js
@@ -0,0 +1,8 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function () {
+    $("#dataTable").DataTable();
+});
+// Call the dataTables jQuery plugin
+$(document).ready(function () {
+    $("#dataTable1").DataTable();
+});
