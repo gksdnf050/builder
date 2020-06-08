d38947bea926a488e398fe7b8da8cb94a1f9c2f8
201411146 <gksdnf050@naver.com>
2020-06-09 00:25:06 +0900

    # hanul - master브랜치로 merge

--- builder/src/main/java/com/company/controller/AllbaBoardController.java
+++ builder/src/main/java/com/company/controller/AllbaBoardController.java
@@ -3,11 +3,11 @@ package com.company.controller;
 import java.io.File;
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +25,7 @@ import com.company.utils.AllbaUploadFileUtils;
 @RequestMapping("/allba/*")
 public class AllbaBoardController {
 
-	@inject
+	@Inject
 	AllbaBoardService service;
 
 	@Resource(name = "uploadPath")
