d614f5ad3f5154a59b71ab620f0a272e72d474e1
201411146 <gksdnf050@naver.com>
2020-07-02 20:49:24 +0900

    # 수정

--- builder/src/main/java/com/company/controller/AllbaController.java
+++ builder/src/main/java/com/company/controller/AllbaController.java
@@ -1,22 +1,38 @@
 package com.company.controller;
 
 
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.company.service.AllbaManageService;
+
+
 
 @Controller
 @RequestMapping("/allba/*")
 public class AllbaController {
 	
-	
+	@Inject
+	AllbaManageService service;
 	//allba사이트 홈
 	@RequestMapping(value = "/{sitename}", method = RequestMethod.GET)
-	public String home(@PathVariable("sitename")String sitename, Model model) {
+	public String home(@PathVariable("sitename")String sitename, Model model, HttpServletRequest req) {
 		model.addAttribute("sitename", sitename);
+		List<Map<String,String>> category = service.category(sitename);
+		List<Map<String,String>> detail = service.detailcategory(sitename);
+		HttpSession session = req.getSession();
+		session.setAttribute("category", category);
+		session.setAttribute("detail", detail);
 		
 		return "allba/home";
 	}
@@ -30,4 +46,3 @@ public class AllbaController {
 		}
 	
 } 
-
