2c3ddfbed88837f0097aa6a3ddfab8a6a8f950f9
201411146 <gksdnf050@naver.com>
2020-08-14 01:24:38 +0900

    #hanul banenrDTO

---
+++ builder/src/main/java/com/company/dto/BannerDTO.java
@@ -0,0 +1,20 @@
+package com.company.dto;
+
+public class BannerDTO {
+
+	private int bannerid;
+	private String file;
+	
+	public int getBannerid() {
+		return bannerid;
+	}
+	public void setBannerid(int bannerid) {
+		this.bannerid = bannerid;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+}
